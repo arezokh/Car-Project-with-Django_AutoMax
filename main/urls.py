474f22cab6d4cd72402be05f91318966e0d35393
arezokh <160070951+arezokh@users.noreply.github.com>
2024-06-26 15:27:49 +0330

    Add files via upload

---
+++ main/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import main_view, home_view, list_view, listing_view, edit_view, inquire_listing_using_email
+
+urlpatterns = [
+    path('', main_view, name='main'),
+    path('home/', home_view, name='home'),
+    path('list/', list_view, name='list'),
+    path('listing/<str:id>/', listing_view, name='listing'),
+    path('listing/<str:id>/edit/', edit_view, name='edit'),
+    path('listing/<str:id>/inquire/',
+         inquire_listing_using_email, name='inquire_listing'),
+]
